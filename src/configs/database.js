cef166872ebe3060d430a8e5822df7c8449b8666
KCH6937 <adasddasd12@naver.com>
2022-06-16 20:07:39 +0900

    feat: DB 설정

---
+++ src/configs/database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+// sequelize configs
+const sequelize = new Sequelize('wanted', 'user', '1234', {
+    dialect: 'sqlite', // DBMS
+    host: './data/dev.sqlite' // Storage location
+});
+
+module.exports = sequelize;
