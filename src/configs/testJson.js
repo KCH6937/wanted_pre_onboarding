c47c01b59ebc39599c5931e7192e232d8d667d51
KCH6937 <adasddasd12@naver.com>
2022-06-16 20:08:29 +0900

    test: 테스트 더미데이터

---
+++ src/configs/testJson.js
@@ -0,0 +1,86 @@
+const testJsons = {};
+
+testJsons.companyData = [
+    {
+        'name': '원티드랩',
+        'nation': '한국',
+        'area': '서울'
+    },
+    {
+        'name': '네이버',
+        'nation': '한국',
+        'area': '판교'
+    },
+    {
+        'name': '당근마켓',
+        'nation': '한국',
+        'area': '서울'
+    },
+    {
+        'name': '구글',
+        'nation': '미국',
+        'area': '뉴욕'
+    },
+    {
+        'name': '구글코리아',
+        'nation': '한국',
+        'area': '서울'
+    },
+    {
+        'name': '카카오',
+        'nation': '한국',
+        'area': '판교'
+    },
+    {
+        'name': '원티드코리아',
+        'nation': '한국',
+        'area': '부산'
+    }
+];
+
+testJsons.jobPostingData = [
+    {
+        'position': '백엔드 주니어 개발자',
+        'compensation': 1000000,
+        'skill': 'Python',
+        'about': '원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요견은 ...'
+    },
+    {
+        'position': 'Backend 시니어 개발자',
+        'compensation': 1000000,
+        'skill': 'Java',
+        'about': '네이버에서 Backend 시니어 개발자를 채용합니다. 자격요견은 ...'
+    },
+    {
+        'position': 'Backend 신입 개발자',
+        'compensation': 1000000,
+        'skill': 'Node.js',
+        'about': '당근마켓에서 Backend 신입 개발자를 채용합니다. 자격요견은 ...'
+    },
+    {
+        'position': 'Backend 10년차 개발자',
+        'compensation': 2000000,
+        'skill': 'Go',
+        'about': '구글(코리아)에서 Backend 10년차 개발자를 채용합니다. 자격요견은 ...'
+    },
+    {
+        'position': 'iOS 주니어 개발자',
+        'compensation': 1500000,
+        'skill': 'Swift',
+        'about': '구글에서 iOS 주니어 개발자를 채용합니다. 자격요견은 ...'
+    },
+    {
+        'position': 'Android 3년차 개발자',
+        'compensation': 1000000,
+        'skill': 'Java, Android Studio',
+        'about': '카카오에서 Android 3년차 개발자를 채용합니다. 자격요견은 ...'
+    },
+    {
+        'position': '프론트엔드 시니어 개발자',
+        'compensation': 2000000,
+        'skill': 'Javascript, React',
+        'about': '원티드코리아에서 프론트엔드 시니어 개발자를 채용합니다. 자격요견은 ...'
+    }
+];
+
+module.exports = testJsons;
