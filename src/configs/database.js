fa2e9735e5c9c8aff97b8f7108d19228ce2a69fa
KCH6937 <adasddasd12@naver.com>
2022-06-18 02:07:38 +0900

    refactor: 공백 제거

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,9 +1,9 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, Op } = require('sequelize');
 
 // sequelize configs
 const sequelize = new Sequelize('wanted', 'user', '1234', {
     dialect: 'sqlite', // DBMS
-    host: './data/dev.sqlite' // Storage location
+    host: './data/dev.sqlite', // Storage location
 });
 
 module.exports = sequelize;
