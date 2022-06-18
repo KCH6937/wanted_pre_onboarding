91bb0e60504cbe04ef189ebd55caeb532d20e099
KCH6937 <adasddasd12@naver.com>
2022-06-18 22:10:05 +0900

    fix: 회사 삭제 API 오류 해결

--- src/routers/companyRouter.js
+++ src/routers/companyRouter.js
@@ -5,7 +5,7 @@ const companyController = require('../controllers/companyController');
 
 router.post('/create', companyController.create); // 회사 등록
 
-router.delete('/delete', companyController.delete); // 회사 삭제
+router.delete('/delete/:id', companyController.delete); // 회사 삭제
 
 router.get('/findAll', companyController.findAll); // 회사 전체목록 불러오기
 
