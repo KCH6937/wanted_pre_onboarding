29112abff77b001789a9c4b67fd98126f200c9fb
KCH6937 <adasddasd12@naver.com>
2022-06-18 22:01:50 +0900

    fix: 서버 실행시 테스트코드 에러 해결

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const request = require('request'); // api 호출 라이브러리
 const app = express();
 const port = 3306;
 
-sequelize.sync({ force: true }).then(() => console.log('DB is now ready')); // sync({ force: true }) => 서버 실행마다 DB 초기화 - 테스트 데이터 충돌 방지 예뱡
+sequelize.sync({ force: true }).then(() => {
+    request({url: `http://localhost:${port}/test/create`,method: 'POST'}); // 서버 시작 시 채용공고, 회사, 유저 자동생성
+    console.log('DB is now ready');
+}); // sync({ force: true }) => 서버 실행마다 DB 초기화 - 테스트 데이터 충돌 방지 예뱡
 
 // static files 
 app.use(express.json());
@@ -18,8 +21,6 @@ app.use('/job', require('./src/routers/jobRouter'));
 app.use('/applyHistory', require('./src/routers/applyHistoryRouter'));
 app.use('/test', require('./src/routers/testRouter'));
 
-request({url: `http://localhost:${port}/test/create`,method: "POST"}); // 서버 시작 시 채용공고, 회사, 유저 자동생성
-
 // Error middleware
 app.use((err, req, res, next) => {
     console.log(err);
@@ -30,3 +31,4 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
+
